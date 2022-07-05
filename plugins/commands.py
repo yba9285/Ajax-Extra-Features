04b893846980864e98c75d42c19f13076b18292e
yba9285 <108516941+yba9285@users.noreply.github.com>
2022-07-05 17:15:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -47,7 +47,7 @@ async def start(client, message):
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
             ],[
-            InlineKeyboardButton('✅ SEARCH ✅', url='switch_inline_query_current_chat=')
+            InlineKeyboardButton('✅ SEARCH ✅', switch_inline_query_current_chat='query')
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)        
         await message.reply_photo(
@@ -90,7 +90,7 @@ async def start(client, message):
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
             ],[
-            InlineKeyboardButton('✅ SEARCH ✅', url='switch_inline_query_current_chat=')
+            InlineKeyboardButton('✅ SEARCH ✅', switch_inline_query_current_chat='query')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
