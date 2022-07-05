f299ebef78c7aca8a5f01358d1a9fefee6e57bfa
yba9285 <108516941+yba9285@users.noreply.github.com>
2022-07-05 16:32:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -753,7 +753,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
             ],[
-            InlineKeyboardButton('✅ SUBSCᏒIBΞ  ✅', url='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA')
+            InlineKeyboardButton('✅ SEARCH  ✅', url='switch_inline_query_current_chat=')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
